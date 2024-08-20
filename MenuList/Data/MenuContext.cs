2871bb903fa45529d2cfccdfdc28c6a652b0ab87
demo <jadavmayur800@gmail.comgit config --listgit config --global user.email jadavmayur800@gmail.com>
2024-08-20 09:36:29 +0530

    Add project files.

---
+++ MenuList/Data/MenuContext.cs
@@ -0,0 +1,43 @@
+﻿using MenuList.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace MenuList.Data
+{
+    public class MenuContext:DbContext
+    {
+        public MenuContext(DbContextOptions<MenuContext> options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishIngredient>().HasKey(di=>new
+            {
+                di.DishId,
+                di.IngredientId
+            });
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+            modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(i => i.DishIngredients).HasForeignKey(i => i.IngredientId);
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish { Id = 1, Name = "Margheritta", Price = 7.50, ImageUrl = "https://images.ctfassets.net/nw5k25xfqsik/64VwvKFqxMWQORE10Tn8pY/200c0538099dc4d1cf62fd07ce59c2af/20220211142754-margherita-9920.jpg" }
+                );
+            modelBuilder.Entity<Ingredient>().HasData(
+                new Ingredient { Id = 1, Name = "Tomato Sauce" },
+                new Ingredient { Id = 2, Name = "Mozzarella" }
+                );
+            modelBuilder.Entity<DishIngredient>().HasData(
+                new DishIngredient { DishId = 1, IngredientId = 1 },
+                new DishIngredient { DishId = 1, IngredientId = 2 }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public Microsoft.EntityFrameworkCore.DbSet<Dish> Dishes { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Ingredient> Ingredients { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<DishIngredient> DishIngredients { get; set; }
+    }
+   
+}
